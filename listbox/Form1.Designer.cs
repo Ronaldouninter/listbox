f80b85abad26c49cca28ecc7a57196020dbc1367
Minilab-ing10 <Minilab-ing10@MINILAB-ING10>
2025-10-20 08:22:42 -0600

    Agregar archivos de proyecto.

---
+++ listbox/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace listbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxAnimales = new System.Windows.Forms.ListBox();
+            this.PictureBoxAnimales = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAnimales)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListBoxAnimales
+            // 
+            this.ListBoxAnimales.FormattingEnabled = true;
+            this.ListBoxAnimales.Location = new System.Drawing.Point(71, 12);
+            this.ListBoxAnimales.Name = "ListBoxAnimales";
+            this.ListBoxAnimales.Size = new System.Drawing.Size(176, 121);
+            this.ListBoxAnimales.TabIndex = 0;
+            this.ListBoxAnimales.SelectedIndexChanged += new System.EventHandler(this.ListBoxAnimales_SelectedIndexChanged);
+            // 
+            // PictureBoxAnimales
+            // 
+            this.PictureBoxAnimales.Location = new System.Drawing.Point(71, 181);
+            this.PictureBoxAnimales.Name = "PictureBoxAnimales";
+            this.PictureBoxAnimales.Size = new System.Drawing.Size(176, 175);
+            this.PictureBoxAnimales.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PictureBoxAnimales.TabIndex = 1;
+            this.PictureBoxAnimales.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 395);
+            this.Controls.Add(this.PictureBoxAnimales);
+            this.Controls.Add(this.ListBoxAnimales);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxAnimales)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxAnimales;
+        private System.Windows.Forms.PictureBox PictureBoxAnimales;
+    }
+}
+
---
+++ listbox/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListBoxAnimales.Items.Add("Perro");
+            ListBoxAnimales.Items.Add("Gato");
+            ListBoxAnimales.Items.Add("Conejo");
+            ListBoxAnimales.Items.Add("Pez");
+        }
+
+        private void ListBoxAnimales_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string animal = ListBoxAnimales.SelectedItem.ToString();
+
+            switch (animal) 
+            {
+                case "Perro":
+                    PictureBoxAnimales.Image= Image.FromFile("C:\\Users\\minilab-ing10\\source\\repos\\listbox\\listbox\\perro.jpg");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
